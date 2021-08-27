d9eb615b6e45e045501168a89e7c43e24d90f7f9
KuebV <42746052+KuebV@users.noreply.github.com>
2021-08-27 11:06:09 -0600

    Added Output.cs
    Allows for easier console logs to be made,

---
+++ ServerAuthentication/ServerAuthentication/Output.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAuthentication
+{
+    public class Output
+    {
+        public static void Message(OutputType type, dynamic message)
+        {
+            Console.WriteLine(string.Format("[LoginAuthentication][{0}] - {1}", Enum.GetName(type.GetType(), type), message));
+        }
+
+    }
+
+    public enum OutputType
+    {
+        Info = 1,
+        Error = 2,
+        Debug =3
+    }
+}
