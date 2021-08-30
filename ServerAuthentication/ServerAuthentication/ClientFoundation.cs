8bb3b233be90a304c6d45f2a25c8e4a5fac5a0ba
KuebV <42746052+KuebV@users.noreply.github.com>
2021-08-30 13:43:08 -0600

    Added Client Foundation
    Parsing Data

---
+++ ServerAuthentication/ServerAuthentication/ClientFoundation.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using ServerAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAuthentication
+{
+    public class ClientFoundation
+    {
+        public ClientFoundation(string JsonData)
+        {
+            JsonClientData = JsonData;
+        }
+
+        private string JsonClientData;
+
+        /// <summary>
+        /// Get Data from the Json Variable
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string GetValueFromJson(ClientDatabaseEnum value)
+        {
+            ClientDatabase clientData = JsonConvert.DeserializeObject<ClientDatabase>(JsonClientData);
+            switch (value)
+            {
+                case ClientDatabaseEnum.AuthenticationString:
+                    return clientData.AuthenticationString;
+                case ClientDatabaseEnum.Hostname:
+                    return clientData.Hostname;
+                case ClientDatabaseEnum.IPAddress:
+                    return clientData.IPAddress;
+                case ClientDatabaseEnum._id:
+                    return clientData._id;
+            }
+
+            return null;
+        }
+    }
+}
