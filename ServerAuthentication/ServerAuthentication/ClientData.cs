af9442d30974287f9799e4601196d510b61e366c
KuebV <42746052+KuebV@users.noreply.github.com>
2021-08-31 10:46:17 -0600

    Update ClientData & More Spaghetti Code

--- ServerAuthentication/ServerAuthentication/ClientData.cs
+++ ServerAuthentication/ServerAuthentication/ClientData.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JsonDB;
+using LoginAuthentication;
+using Newtonsoft.Json.Linq;
 
 namespace ServerAuthentication
 {
@@ -13,18 +15,16 @@ namespace ServerAuthentication
         /// Private Integer for the ClientID for incoming client login
         /// </summary>
         private int ClientID;
-        
+
         /// <summary>
         /// Settings for the ClientAuthenticaton
         /// </summary>
         private bool RequireIPMatch;
-        
+
         /// <summary>
         /// Settings for the ClientAuthenticaton
         /// </summary>
         private bool RequireHostnameMatch;
-
-        private Server _server;
 
         /// <summary>
         /// Constructor for the Client Data Class
@@ -45,15 +45,48 @@ namespace ServerAuthentication
         /// <param name="Hostname"></param>
         /// <param name="AuthString"></param>
         /// <returns></returns>
-        public LoginResponse HandleLogin(string IP, string Hostname, string AuthString)
+        public LoginResponse HandleLoginData(string IP, string Hostname, string AuthString, string ClientIdentity)
         {
             // If the Server is running off of the Local Storage Option
-            if (_server.m_LocalStorageEnabled)
+            if (Server.m_LocalStorageEnabled)
             {
                 // Create the Server-Side Hash
                 string stringBuilder = string.Format("{0},{1}", IP, Hostname);
                 string ServerHash = Server.ComputeSha256Hash(stringBuilder);
-    
+
+                Item item = Database.item;
+
+                var clientData = item.FindItem(ClientIdentity);
+                ClientFoundation clientFoundation = new ClientFoundation(clientData.ToString());
+
+                #region Locally Stored Client Database
+                string localIP = clientFoundation.GetValueFromJson(ClientDatabaseEnum.IPAddress);
+                string localHostName = clientFoundation.GetValueFromJson(ClientDatabaseEnum.Hostname);
+                string localAuthenticationString = clientFoundation.GetValueFromJson(ClientDatabaseEnum.AuthenticationString);
+                #endregion
+
+                #region Check Authentication String
+                if (ServerHash != localAuthenticationString)
+                    return LoginResponse.ServerError;
+
+                if (ServerHash != AuthString)
+                    return LoginResponse.Failed;
+                #endregion
+
+                #region 
+                if (RequireIPMatch && IP == localIP && RequireHostnameMatch && Hostname == localHostName)
+                    return LoginResponse.Good;
+
+                if (RequireHostnameMatch && Hostname != localHostName)
+                    return LoginResponse.HostNameMatch_False;
+
+                if (RequireIPMatch && IP != localIP)
+                    return LoginResponse.IPMatch_False;
+
+                if (!RequireIPMatch && !RequireHostnameMatch)
+                    return LoginResponse.Good;
+
+
             }
 
             return LoginResponse.Unknown;
@@ -91,6 +124,7 @@ namespace ServerAuthentication
         Failed = 1,
         IPMatch_False = 2,
         HostNameMatch_False = 3,
-        Unknown = 4
+        ServerError = 4,
+        Unknown = 5
     }
 }
